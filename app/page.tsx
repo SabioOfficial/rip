e7241557bb48ccb098f43ba99f9839e5c0ca1d20
Sabio <gamester.sad@gmail.com>
2025-12-04 10:10:19 +0000

    update UserRate -> UserInfo name + add project information to all descriptions

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,10 @@ export default function Home() {
           <Image src="/obserpoint-logo-or-something.svg" alt="wow look at this little green circle.. now close your eyes and press Alt+F4..." width={32} height={32}></Image>
           <h3 className="text-3xl">Obserpoint</h3>
         </div>
-        <p>There wasn't a <span className="text-xl">point</span> of continuing this... get it? Obser<span className="text-xl">point</span>? No? FUCK</p>
+        <p>
+          There wasn't a <span className="text-xl">point</span> of continuing this... get it? Obser<span className="text-xl">point</span>? No? FUCK! 
+          anyways this project was about tracking website uptime and was supposed to be for Quntem.
+        </p>
         <div className="flex flex-col gap-1 p-4 border-2">
           <p><span className="text-xl">discontinuation reason:</span> code and shit was very jumbled and messy and it wasnt interesting enough for me to continue :D</p>
           <p><span className="text-xl">would i work on this again:</span> <span className="text-red-600">HELL NO</span></p>
@@ -29,7 +32,10 @@ export default function Home() {
           <Image src="/weird-ass-clock.jpg" alt="who the fuck draws this (alarm clock with detailed feet and hands btw)" width={32} height={32}></Image>
           <h3 className="text-3xl">Timeset</h3>
         </div>
-        <p>The app looked as ugly as you (very ugly) and got me demotivated NOOOooOOOOOOooooO also it was a stupid fucking idea</p>
+        <p>
+          The app looked as ugly as you (very ugly) and got me demotivated NOOOooOOOOOOooooO also it was a stupid fucking idea. 
+          so the app was supposed to be an alternative to the clock app (which nobody uses :skulk:) so like oops 10 hours down the drain
+        </p>
         <div className="flex flex-col gap-1 p-4 border-2">
           <p><span className="text-xl">discontinuation reason:</span> burnt out + shit idea </p>
           <p><span className="text-xl">would i work on this again:</span> <span className="text-red-600">FUCK NO</span></p>
@@ -39,9 +45,12 @@ export default function Home() {
       <div className="flex flex-col gap-4">
         <div className="flex flex-row gap-2 items-center">
           <Image src="/slackbot-happy-idk.png" alt="slackbot very happy png i found on the internyet" width={32} height={32}></Image>
-          <h3 className="text-3xl">UserRate</h3>
+          <h3 className="text-3xl">UserInfo <span className="text-xl">(formerly UserRate)</span></h3>
         </div>
-        <p>i couldn't finish this in time for the summer of making "game jam" as other projects had priority idk what you fucking call it</p>
+        <p>
+          i couldn't finish this in time for the summer of making "game jam" as other projects had priority idk what you fucking call it. 
+          it was originally meant to be tea for hack club but now it's just user information checker!
+        </p>
         <div className="flex flex-col gap-1 p-4 border-2">
           <p><span className="text-xl">discontinuation reason:</span> other projects were prioritized + ran out of time</p>
           <p><span className="text-xl">would i work on this again:</span> <span className="text-green-500">YES!</span></p>
